74cc1a5c2220c05649481042c268f976cb9e91f0
John M. Wargo <johnwargo@users.noreply.github.com>
2024-10-26 13:05:27 -0400

    Update KeychainSkimmerScanner.ino
    Fixed typo in error message

--- KeychainSkimmerScanner.ino
+++ KeychainSkimmerScanner.ino
@@ -11,7 +11,7 @@
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#error Bluetooth is not enabled! Please run `make menuconfig` to enable it
 #endif
 
 #if !defined(CONFIG_BT_SPP_ENABLED)
